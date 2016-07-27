89ac80317c83a217a69ff0027c5d553168862a9e
Arnstein <arnstej@gmail.com>
2016-07-27 19:22:58 +0200

    Added fonts

--- src/TrilleLille/TrilleLille.Web/Models/GroupViewModels/CreateGroupViewModel.cs
+++ src/TrilleLille/TrilleLille.Web/Models/GroupViewModels/CreateGroupViewModel.cs
@@ -1,18 +1,23 @@
-﻿using Models;
+﻿using System.ComponentModel.DataAnnotations;
+using Models;
 
 namespace TrilleLille.Web.Models.GroupViewModels
 {
     public class CreateGroupViewModel
     {
+        [Display(Name = "Gruppens navn")]
         public string Name { get; set; }
 
         public int LocationId { get; set; }
 
+        [Display(Name = "En kort beskrivelse om gruppen")]
         public string GroupIntro { get; set; }
         public string CreatorId { get; set; }
 
+        [Display(Name = "Ditt navn")]
         public string CreatorName { get; set; }
 
+        [Display(Name = "Fortell oss litt om deg selv")]
         public string CreatorBio { get; set; }
         
         public Gender CreatorGender { get; set; }
--- src/TrilleLille/TrilleLille.Web/gulpfile.js
+++ src/TrilleLille/TrilleLille.Web/gulpfile.js
@@ -1,5 +1,22 @@
-var gulp = require('gulp');
+var gulp = require('gulp'), watch = require('gulp-watch');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', ['sass'], function () {
+    return;
+});
+gulp.task('watch', ['watch'], function () {
+    return;
+});
+
+gulp.task('sass', function() {
+        gulp.src('./Sass/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./wwwroot/css/'))
+        .pipe(reload({stream:true}));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./Sass/*.scss', [sass]);
 });
